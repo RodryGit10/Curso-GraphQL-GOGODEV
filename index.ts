43401f3e3c612126eda76d41a70f0a490bcca224
Rodrigo <simrodri10@gmail.com>
2024-03-30 12:27:01 -0400

    Configuración De Un Servidor De Suscripciones

--- index.ts
+++ index.ts
@@ -1,14 +1,51 @@
-import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
-import { typeDefs } from "./graphql/typeDefs.js"
-import { resolvers } from "./graphql/resolvers.js"
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import express from "express";
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4';
+import bodyParser from 'body-parser'
 
+const port = 4000
 
-const server = new ApolloServer({
-    typeDefs, resolvers
+const typeDefs = `
+  
+`
+
+const resolvers = {
+
+}
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+const app = express()
+const httpServer = createServer(app)
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
+})
+const wsServerClean = useServer({schema}, wsServer)
+
+const apolloServer = new ApolloServer({
+  schema,
+  plugins: [
+    ApolloServerPluginDrainHttpServer({httpServer}),
+    {
+      async serverWillStart() {
+        return {
+          async drainServer(){
+            await wsServerClean.dispose()
+          }
+        }
+      }
+    }
+  ]
 })
 
-const { url } = await startStandaloneServer(server, {
-    listen: {port: 4000}
+await apolloServer.start()
+
+app.use('/graphql', bodyParser.json(), expressMiddleware(apolloServer));
+httpServer.listen(port, () => {
+  console.log(`listening on port: http://localhost:${port}/graphql`)
 })
-console.log(`Server ready at ${url}`)
